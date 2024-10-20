df2481308d01546477361f85682537722c219f0f
unkofire <146385371+unkofire@users.noreply.github.com>
2024-10-19 17:22:58 -0700

    SHREW-10
    Generates a response text based on the prompt "Explain how a computer works to a child". Basic code for gemini testing.

--- Testing_Files/gemini_testing.py
+++ Testing_Files/gemini_testing.py
@@ -1,1 +1,11 @@
-python 3.9+ required
+# python 3.9+ required
+import google.generativeai as genai #Make sure you have generative ai downloaded.
+# Do pip3 install --upgrade google.generativeai
+
+genai.configure(api_key="AIzaSyBUKLB2f4yuLoNxx4WUmYs1tGZWj-NUqdY") # Configures api key.
+
+model = genai.GenerativeModel('gemini-1.5-flash-latest') # Uses the gemini model we are working with.
+
+response = model.generate_content("Explain how a computer works to a child.") # sends a prompt to the AI.
+
+print(response.text) # AI generates a response and is stored in response as text. 
