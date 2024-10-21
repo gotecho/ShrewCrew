783c26f8c6878467deac8ab4c00043242fcd6865
sheadspeth <spencerheadspeth@csus.edu>
2024-10-20 20:09:45 -0700

    Allows Gemini to take user input and give a repsonse

--- Testing_Files/gemini_testing.py
+++ Testing_Files/gemini_testing.py
@@ -1,11 +1,13 @@
 # python 3.9+ required
 import google.generativeai as genai #Make sure you have generative ai downloaded.
-# Do pip3 install --upgrade google.generativeai
+# Do pip3 install --upgrade google.generativeaih
 
 genai.configure(api_key="API_KEY") # Configures api key.
 
 model = genai.GenerativeModel('gemini-1.5-flash-latest') # Uses the gemini model we are working with.
 
-response = model.generate_content("Explain how a computer works to a child.") # sends a prompt to the AI.
+user_prompt = input("Please enter your prompt: ")  # Prompt the user for input using the keyboard
 
-print(response.text) # AI generates a response and is stored in response as text. 
+response = model.generate_content(user_prompt)  # Sends the user-provided prompt to the AI.
+
+print(response.text)  # AI generates a response and is stored in response as text.
