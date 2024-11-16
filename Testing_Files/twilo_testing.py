c2ef28117e8cc14c37af943b91e042811a922c25
unkofire <146385371+unkofire@users.noreply.github.com>
2024-11-15 18:14:50 -0800

    SHREW-17
    Created code to test if it works. Uses ngrok to create a local link. Twilio forwards message to the link and flask handles POST request and responds with a message.

--- Testing_Files/twilo_testing.py
+++ Testing_Files/twilo_testing.py
@@ -2,10 +2,12 @@
 #ENSURE YOU HAVE TWILIO INSTALLED IN YOUR MACHINE! THIS PROGRAM WON'T WORK OTHERWISE!
 #Not sure how to get around this.
 import os
+from dotenv import load_dotenv
 from flask import Flask, request
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 import gemini_testing
+load_dotenv()
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 app = Flask(__name__)
@@ -20,16 +22,23 @@ client = Client(account_sid, auth_token)
 @app.route("/sms", methods=['POST'])
 def sms_reply():
     incoming_message = request.form['Body']
+    print(f"Incoming message: {incoming_message}")
     gemini_response = gemini_testing.generate_gemini_response(incoming_message) # Trying to send twilio message to gemini
     resp = MessagingResponse()
     resp.message(gemini_response)
-    print(resp)
+    print(f"twilio response: {str(resp)}")
     return str(resp)
 
+twilio_phone_number = "+1 916 914 8824"
 
-message = client.messages.create(
-    body="Join Earth's mightiest heroes. Like Kevin Bacon.", #Example Message, change to whatever you want to send
-    from_="+1 916 914 8824", #Number provided by Sacramento City
-    to="+1 831 235 2665", #my own number, can change to your number if you need to for testing
-)
+webhook_url = 'https://556a-98-41-39-122.ngrok-free.app/sms' # My ngrok local url with flask's /sms
+client = Client(account_sid, auth_token)
 
+phone_number = client.incoming_phone_numbers.list(phone_number=twilio_phone_number)[0]
+phone_number.update(sms_url=webhook_url) # lists phone numbers associated with twilio account. 
+# Updates webhook URL that wilio will use when a message is received at provided phone number.
+
+print(f"Webhook URL for {twilio_phone_number} updated to {webhook_url}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
