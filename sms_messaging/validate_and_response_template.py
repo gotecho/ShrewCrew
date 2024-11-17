6696c9980f3e4387cc9cca29839f9bd5f800e6ea
ecimatu <126529280+ecimatu@users.noreply.github.com>
2024-11-17 15:55:51 -0800

    SHREW-54
    Attempted implementation of service request conversational flow

--- sms_messaging/validate_and_response_template.py
+++ sms_messaging/validate_and_response_template.py
@@ -5,22 +5,24 @@ from msg_receive import fetch_sms
 
 app = Flask(__name__)
 
-client = Client(twilio_acct_info.acct_sid, twilio_acct_info.acct_auth_tok)
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+service_request_session = {}
 
 # Dictionary of responses
 animal_care_request_options = {
     'dead animal': 'Thank you for reporting the sighting of a dead animal on '
-    + 'a public right-of-way. We\'ll get an Animal Control Officer to respond '
-    + 'to your service request as soon as possible.',
+        + 'a public right-of-way. We\'ll get an Animal Control Officer to respond '
+        + 'to your service request as soon as possible.',
     'pet complaint or concern': 'Thank you for reporting your complaint '
-    + 'or concern for an owned animal. Please provide the following information: ',
+        + 'or concern for an owned animal. Please provide the following information: ',
     'stray or loose animal' : 'Thank you for reporting a stray or loose animal. '
-    + 'We\'ll get an Animal Control Officer to respond as soon as possible.',
+        + 'We\'ll get an Animal Control Officer to respond as soon as possible.',
     'animal shelter' : 'Questions about adoptions, spay/neuter resources, '
-    + 'found animals, licensing and barking are directed to the Front Street '
-    + 'Shelter. Please provide the following information: ',
+        + 'found animals, licensing and barking are directed to the Front Street '
+        + 'Shelter. Please provide the following information: ',
     'animal control' : 'Thank you for reporting your animal control '
-    + 'request. Please provide the following information: '
+        + 'request. Please provide the following information: '
 }
 
 def verify_service_request(message):
@@ -31,21 +33,58 @@ def verify_service_request(message):
 def request_response():
     received_sms = request.form.get('Body', '').strip().lower()
     # Option to get and retain user phone numbers for database purposes
-    # user_phone_number = request.form.get('From', '') 
+    user_phone_number = request.form.get('From', '') 
     resp = MessagingResponse()
 
-    if verify_service_request(received_sms):
-        for keyphrase, response_msg in animal_care_request_options.items():
-            if keyphrase in received_sms:
-                resp.message(response_msg)
-                break
-            else:
-                resp.message('Thank you for using Sacramento\'s 311 text '
-                         + 'channel. For a list of Animal Care Request '
-                         + 'options, please type "menu".')
-    else:
-        resp.message('Your message is not a valid service request. Please '
-                     + 'use the following format: "Service Request: '
-                      + '[your service request]"')
+    # Need to figure out how to implement multiple reports from one number
+    if user_phone_number in service_request_session:
+        curr_session = service_request_session[user_phone_number]['state']
+        if curr_session == 'active':
+            resp.message(
+                'You currently have an active service '
+                + 'request. What information do you wish to know? '
+                + 'Please, select from the following: ')
+                # link follow up items
+        elif curr_session == 'awaiting_animal_info':
+            resp.message(
+                'Thank you for providing needed additional information. '
+                + 'Is there anything else you\'d like to report?'
+                )
+            curr_session[user_phone_number]['state'] = 'completed'
+        elif curr_session == 'completed':
+            resp.message(
+                'Thank you for using Sacramento\'s 311 text channel. '
+                + 'You will momentarily receive a message regarding '
+                + 'your level(s) of satisfaction'
+                )
+        elif received_sms == 'done':
+            resp.message(
+                'Thank you for using Sacramento\'s 311 text channel. '
+                + 'Goodbye!'
+                )
+            del curr_session[user_phone_number]
+
+    else:    
+        if verify_service_request(received_sms):
+            # animal_care_options can be switched out for another service type
+            # need to figure out how to better implement service type handling
+            for keyphrase, response_msg in animal_care_request_options.items():
+                if keyphrase in received_sms:
+                    resp.message(response_msg)
+                    details = {'next_details' : 'awaiting_animal_info'}
+                    service_request_session[user_phone_number] = {'state': details['next_details']}
+                    break
+                else:
+                    resp.message(
+                        'Thank you for using Sacramento\'s 311 text '
+                        + 'channel. For a list of Animal Care Request '
+                        + 'options, please type "menu".'
+                        )
+        else:
+            resp.message(
+                'Your message is not a valid service request. Please '
+                + 'use the following format: "Service Request: '
+                + '[your service request]"'
+                )
     
     return Response(str(resp), mimetype = 'text/xml')
